8fe11e2ec8c0a3308b4d803dfad0086a5fbb0779
jose <jozuniga.t@gmail.com>
2020-06-23 02:17:40 -0400

    Se crea file skills.json y se actualiza file js para carga dinamica en index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,16 @@ function load() {
   document.getElementById("show").style.display = "none";
   var anio = new Date().getFullYear();
   document.getElementById("anio").innerHTML = "(" + anio + ")";
-  var edad =this.calcularEdad("1992/04/02");
+  var edad = this.calcularEdad("1992/04/02");
   document.getElementById("edad").innerHTML = edad + " años";
+
+  this.obtenerSkills();
 }
 
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
@@ -19,7 +21,7 @@ var currentScrollPos = window.pageYOffset;
 }
 
 
-function showResumen(){
+function showResumen() {
   var content = document.getElementById("content_resumen");
   if (content.style.display === "block" || content.style.display === "") {
     content.style.display = "none";
@@ -41,9 +43,33 @@ function calcularEdad(fecha) {
   var m = hoy.getMonth() - cumpleanos.getMonth();
 
   if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
-      edad--;
+    edad--;
   }
 
   return edad;
 }
 
+function obtenerSkills() {
+
+  fetch("./assets/data/skills.json")
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (data) {
+      var skills = data[0];
+      var html = '';
+      for (i in skills) {
+        html += `
+              <div class="col mb-4">
+              <div class="card skills" style="text-align: center;">
+                <img src="${skills[i].img}" class="" alt="..." style="width: 80px;">
+                <div class="card-body"  style="justify-content: center;">
+                  <h7 class="card-title">${skills[i].nombre}</h7>
+                </div>
+              </div>
+            </div>
+          `;
+      }
+      document.getElementById("skills").innerHTML = html;
+    });
+}
